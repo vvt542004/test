93dabc0d94065cc6f6ef5287fcb87a5ee0abad8a
vvt542004 <22010046@st.phenikaa-uni.edu.vn>
2024-05-07 20:47:20 +0700

    slide1

--- scrip.js
+++ scrip.js
@@ -28,3 +28,17 @@ leftbtn.addEventListener("click", function(){
     }
     document.querySelector(".slider-content-left-top").style.right = index * 100+"%"
 })
+
+const imgNuberLi = document.querySelectorAll('.slider-content-left-bottom li')
+imgNuberLi.forEach(function(image,index){
+    image.addEventListener("click", function(){
+        removeactive ()
+        document.querySelector(".slider-content-left-top").style.right = index * 100+"%"
+        image.classList.add("active")
+    })
+
+})
+function removeactive (){
+    let imgactive = document.querySelector('.active')
+    imgactive.classList.remove("active")
+}
