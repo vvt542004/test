44053762e753c84d5df5dd695f944d70a35b6bf4
vvt542004 <22010046@st.phenikaa-uni.edu.vn>
2024-05-09 18:36:03 +0700

    slide-product2

--- scrip.js
+++ scrip.js
@@ -42,3 +42,21 @@ function removeactive (){
     let imgactive = document.querySelector('.active')
     imgactive.classList.remove("active")
 }
+//----slide-product----
+const rightbtntwo = document.querySelector('.fa-chevron-right-two')
+const leftbtntwo = document.querySelector('.fa-chevron-left-two')
+const imgNubertwo = document.querySelectorAll('.slide-product-one-content-items')
+rightbtntwo.addEventListener("click", function(){
+    index = index + 1
+    if(index>imgNubertwo.length-1){
+        index=0
+    }
+    document.querySelector(".slide-product-one-content-items-content").style.right = index * 100+"%"
+})
+leftbtntwo.addEventListener("click", function(){
+    index = index-1
+    if(index<=0){
+        index=imgNubertwo.length-1
+    }
+    document.querySelector(".slide-product-one-content-items-content").style.right = index * 100+"%"
+})
